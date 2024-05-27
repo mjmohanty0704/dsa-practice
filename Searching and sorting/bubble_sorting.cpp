ae5bbe034f57b5dd7b341c7286bae527da5bf7fe
Manoj <119852764+mjmohanty0704@users.noreply.github.com>
2024-05-27 09:54:37 +0000

    implemented bubble sort and insertion sort

---
+++ Searching and sorting/bubble_sorting.cpp
@@ -0,0 +1,58 @@
+/*
+Push the max element to the last by adjacent swaps
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// Function to perform selection sort on an array
+void bubble_sort(int arr[], int n)
+{
+    for (int i = n - 1; i >= 0; i--)
+    {
+        int did_swap = 0;
+        for (int j = 0; j <= i - 1; j++)
+        {
+            if (arr[j] > arr[j + 1])
+            {
+                int temp = arr[j + 1];
+                arr[j + 1] = arr[j];
+                arr[j] = temp;
+                did_swap = 1;
+            }
+            if (!did_swap == 0)
+                break;
+        }
+    }
+}
+
+int main()
+{
+    int n;
+
+    // Input the size of the array
+    cout << "Enter the number of elements: ";
+    cin >> n;
+
+    int arr[n];
+
+    // Input the elements of the array
+    cout << "Enter the elements of the array: ";
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+
+    // Perform selection sort on the array
+    bubble_sort(arr, n);
+
+    // Output the sorted array
+    cout << "Sorted array: ";
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
---
+++ Searching and sorting/insertion-sorting.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Function to perform bubble sort on an array
+void insertion_sort(int arr[], int n)
+{
+    for (int i = 0; i <= n - 1; i++)
+    {
+        int j = i;
+        while (j > 0 && arr[j - 1] > arr[j])
+        {
+            int temp = arr[j - 1];
+            arr[j - 1] = arr[j];
+            arr[j] = temp;
+            j--;
+        }
+    }
+}
+
+int main()
+{
+    int n;
+
+    // Input the size of the array
+    cout << "Enter the number of elements: ";
+    cin >> n;
+
+    int arr[n];
+
+    // Input the elements of the array
+    cout << "Enter the elements of the array: ";
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+
+    // Perform bubble sort on the array
+    insertion_sort(arr, n);
+
+    // Output the sorted array
+    cout << "Sorted array: ";
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
